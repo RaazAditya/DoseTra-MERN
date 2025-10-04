48eb83a299a16a73f2d380dff8a0de47a5f938bb
Anisha Nigam <anishanigam17@gmail.com>
2025-10-04 15:03:28 +0530

    Implement notification system with email and browser push notifications; add subscription model and cron job for reminders

--- backend/routes/notificationsRoutes.js
+++ backend/routes/notificationsRoutes.js
@@ -1,8 +1,11 @@
 import express from "express";
-import { sendTestNotification } from "../controllers/notificationController.js";
+import { sendTestNotification, subscribeNotification } from "../controllers/notificationController.js";
+import authMiddleware from "../middleware/authMiddlewares.js";
 
 const router = express.Router();
 
-router.post("/test", sendTestNotification);
+router.post("/test",authMiddleware, sendTestNotification);
+router.post("/subscribe",authMiddleware, subscribeNotification);
 
 export default router;
+
